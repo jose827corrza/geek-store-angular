1d9a9b6e7e4cea8bb0149536510c1e0d8c916779
Jose Corredor <jose96corrza@gmail.com>
2023-11-07 00:45:18 -0500

    :rocket: Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ImgComponent } from './componentes/img/img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/componentes/img/img.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styles: [
+  ]
+})
+export class ImgComponent {
+
+}
